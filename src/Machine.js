6fd008b4d126a712cfecdb9a2c53731d57346960
PrintHiEgProg <egorkobzev23gmail.com>
2024-08-13 00:26:42 +0300

    + stickers dodo pizza

--- src/Machine.js
+++ src/Machine.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Casino.css";
 
-const fruits = ["🦆", "🪿", "🦜", "🦩", "🐓", "🦢"];
+const fruits = ["🦆", "🦤", "🦜", "🦩", "🐓", "🦢"];
 const prizes = [
   "💰",
   "🥧",
